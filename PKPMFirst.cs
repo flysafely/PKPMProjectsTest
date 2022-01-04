2e64540374b8c05abb8f4e986e457ed0e06cbb52
flysafely <616604060@qq.com>
2022-01-04 19:06:58 +0800

    2022.1.4-测试PKPM二开接口

---
+++ PKPMFirst.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BIMBase.Attributes;
+using BIMBase.ApplicationService;
+using BIMBase.UI;
+using BIMBase.DB;
+
+namespace PKPMFirst
+{
+    [BPExternalCommandAttribute(Name = "Hello")]
+    public class PKPMFirst : IBPFunctionCommand
+    {
+        public override void OnExcute(BPCommandContext context)
+        {
+            MessageBox.Show("Hello World?");
+            base.OnExcute(context);
+        }
+    }
+}
