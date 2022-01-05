e8052e423741ff708dbe8aadb23b34248525a883
flysafely <616604060@qq.com>
2022-01-05 18:54:25 +0800

    2022.1.5-发现部分内部对象无法调用构造函数

--- PKPMFirst.cs
+++ PKPMFirst.cs
@@ -8,16 +8,51 @@ using BIMBase.Attributes;
 using BIMBase.ApplicationService;
 using BIMBase.UI;
 using BIMBase.DB;
+using BIMBase.DB.Geometry;
 
-namespace PKPMFirst
+namespace PKPMFirstProject
 {
     [BPExternalCommandAttribute(Name = "Hello")]
-    public class PKPMFirst : IBPFunctionCommand
+    public class PKPMFirst : IBPPointCommand
     {
-        public override void OnExcute(BPCommandContext context)
+        BPGePoint3d m_ptS;
+        BPGePoint3d m_ptE;
+        int m_nFlag = 0;
+
+        public override bool OnLeftClick(BPCommandContext context)
         {
-            MessageBox.Show("Hello World?");
-            base.OnExcute(context);
+            if (m_nFlag == 0)
+            {
+                m_nFlag = 1;
+                m_ptS = context.MousePoint;
+            }
+            else if (m_nFlag == 1)
+            {
+                m_nFlag = 0;
+                m_ptE = context.MousePoint;
+                BPGeGraphics graphic = createGraphic();
+                graphic.save();
+            }
+            
+            return true;
+        }
+        public override void OnDynamicDraw(BPCommandContext context)
+        {
+            if (m_nFlag == 0) return;
+            m_ptE = context.MousePoint;
+            BPViewport vp = BPApplication.Singleton().ActiveDocument.ViewManager.getSelectedViewport();
+            BPGeGraphics graphic = createGraphic();
+            vp.dynamicDraw(graphic);
+        }
+        public BPGeGraphics createGraphic()
+        {
+            BPViewport vp = BPApplication.Singleton().ActiveDocument.ViewManager.getSelectedViewport();
+            BPModel model = vp.getTargetModel();
+            BPGeGraphics graphic = new BPGeGraphics(model);
+            BPGeSegment3d line = new BPGeSegment3d(m_ptS, m_ptE);
+            //BPArchWall newWall = new BPArchWall(line, 0, 0);
+            graphic.addCurve(BPGeCurveBase.createSegment(line));
+            return graphic;
         }
     }
 }
